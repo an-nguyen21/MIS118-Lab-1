771203c3c8b300ba175cf8a86df7d6d2e91ae6fa
an-nguyen21 <anniengvyen1@gmail.com>
2021-01-07 12:15:31 -0800

    lab1

---
+++ 1.3/1.3/1.3/frnMain.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _1._3
+{
+    partial class frnMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(378, 56);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(90, 23);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(35, 59);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(132, 17);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "What is your Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(173, 56);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(184, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(170, 129);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 17);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frnMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 186);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "frnMain";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ 1.3/1.3/1.3/frnMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._3
+{
+    public partial class frnMain : Form
+    {
+        public frnMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string strname = txtName.Text;
+
+            //Concatenates strname and a string to display a message
+            lblMessage.Text = "Hello World my name is: " + txtName.Text;
+        }
+    }
+}
